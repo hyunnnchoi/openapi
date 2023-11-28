4f5be889d7f328dd346e08a1b6df85f66f62b119
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2023-11-28 20:49:39 +0900

    remove key

---
+++ openapi/Weather.swift
@@ -0,0 +1,27 @@
+//
+//  Weather.swift
+//  openapi
+//
+//  Created by HYUNMOK CHOI on 11/28/23.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let temp_min: Double
+    let temp_max: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
